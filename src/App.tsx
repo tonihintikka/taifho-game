e87e93a55da0e16344bed374b915bbafd6c18bc3
Toni Hintikka <toni.hintikka@gmail.com>
2023-05-02 14:05:02 +0300

    before audit fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,22 @@ const App: React.FC = () => {
     const fromY = 10 - parseInt(from.slice(1), 10);
 const toX = to.charCodeAt(0) - "a".charCodeAt(0);
 const toY = 10 - parseInt(to.slice(1), 10); // Change to[to.length - 1] to to.slice(1)
+  // Tarkista, onko kohderuudussa jo nappula
+  if (boardState[toY][toX] !== null) {
+    console.log("Cannot move to a non-empty square.");
+    return;
+  }
+  const movingPiece = boardState[fromY][fromX];
+  if (movingPiece.pieceType === "C") { // Ympyrä
+    const xDiff = Math.abs(toX - fromX);
+    const yDiff = Math.abs(toY - fromY);
+
+    // Tarkista, onko siirto vain yhden askeleen päässä
+    if (xDiff > 1 || yDiff > 1 || (xDiff === 0 && yDiff === 0)) {
+      console.log("Ympyrä voi liikkua vain yhden askeleen mihin tahansa suuntaan.");
+      return;
+    }
+  }
 
 
     const newBoardState = [...boardState];
